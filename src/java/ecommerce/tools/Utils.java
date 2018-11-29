643abf20fd80bf476bc83a233c746a5f09f62e22
thanhndse <thanhndse@gmail.com>
2018-11-29 13:57:58 +0700

    aa

--- src/java/ecommerce/tools/Utils.java
+++ src/java/ecommerce/tools/Utils.java
@@ -15,7 +15,7 @@ import java.security.NoSuchAlgorithmException;
 public class Utils {
 
     public static String encryptPassword(String passwordText) throws NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        MessageDigest md = MessageDigest.getInstance("SHA256");
         md.update(passwordText.getBytes());
         byte[] bytes = md.digest();
         StringBuilder sb = new StringBuilder();
